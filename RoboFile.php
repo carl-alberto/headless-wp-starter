e7ec799f73350faf22fbf32738e6ed9a340b39ac
Alexi Akl <alexiakl@gmail.com>
2018-04-12 15:55:56 +0300

    feat: ask for db root pass

--- RoboFile.php
+++ RoboFile.php
@@ -27,11 +27,17 @@ class RoboFile extends \Robo\Tasks {
 			return;
 		}
 
-		$this->_exec( "mysql -uroot -proot -h 0.0.0.0 -e 'create user if not exists " . $opts['wp-db-name'] . "'" );
-		$this->_exec( "mysql -uroot -proot -h 0.0.0.0 -e 'create database if not exists " . $opts['wp-db-name'] . "'" );
-		$this->_exec( 'mysql -uroot -proot -h 0.0.0.0 -e "grant all privileges on ' . $opts['wp-db-name']
-		. ' . * to ' . $opts['wp-db-name'] . "@localhost identified by '" . $opts['wp-db-name'] . "'\"" );
-		$this->_exec( "mysql -uroot -proot -h 0.0.0.0 -e 'flush privileges'" );
+		$dbpass = $this->ask("DB root password [root]: ");
+		if ( !$dbpass || strlen( $dbpass ) == 0 ) {
+			$dbpass = "root";
+		}
+
+		$this->_exec( "mysql -uroot -p" . $dbpass . " -h 0.0.0.0 -e 'create user if not exists " . $opts['wp-db-name'] . "'" );
+		$this->_exec( "mysql -uroot -p" . $dbpass . " -h 0.0.0.0 -e 'create database if not exists " . $opts['wp-db-name'] . "'" );
+		$this->_exec( "mysql -uroot -p" . $dbpass . " -h 0.0.0.0 -e \"grant all privileges on " . $opts['wp-db-name']
+		. ".* to " . $opts['wp-db-name'] . "@localhost identified by '" . $opts['wp-db-name'] . "'\"" );
+
+		$this->_exec( "mysql -uroot -p" . $dbpass . " -h 0.0.0.0 -e 'flush privileges'" );
 
 		$this->wp( 'core download --version=4.9.4 --locale=en_US --force' );
 		$this->wp( 'core config --dbname=' . $opts['wp-db-name'] . ' --dbuser=' . $opts['wp-db-name'] . ' --dbpass=' . $opts['wp-db-name'] . ' --dbhost=0.0.0.0' );
